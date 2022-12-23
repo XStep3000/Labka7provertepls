626e9be3d72145f6c7db487b38ff8bf59eb25b49
XStep3000 <kovalevskiymisha625@gmail.com>
2022-12-23 20:35:52 +0300

    На эту работу я потратил кучу времени и хочу извиниться за задержку в её производстве

---
+++ Попытка 2/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+        speed: 2000,
+        parallax: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
